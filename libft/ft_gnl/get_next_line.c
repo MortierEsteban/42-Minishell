c99bfd3d6f52c70fd5135bc7ac8758c8d97c9d9e
Logan Sidan <lsidan@z4r3p7.42lyon.fr>
2022-02-17 13:50:13 +0100

    Add libft

---
+++ libft/ft_gnl/get_next_line.c
@@ -0,0 +1,47 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsidan <lsidan@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 13:48:50 by lsidan            #+#    #+#             */
+/*   Updated: 2022/01/20 14:26:53 by lsidan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+char	*ft_free(char *a)
+{
+	free(a);
+	return (0);
+}
+
+char	*get_next_line(int fd)
+{
+	static char	buff[MAX_FD][BUFFER_SIZE + 1];
+	char		*txt;
+	int			i;
+
+	i = 1;
+	txt = NULL;
+	if (fd < 0 || BUFFER_SIZE == 0)
+		return (0);
+	if (ft_search(buff[fd], '\n') != -1)
+	{
+		ft_cut(buff[fd]);
+		txt = ft_join(txt, buff[fd]);
+		if (txt[0] == '\0')
+			txt = ft_free(txt);
+	}
+	while (ft_search(buff[fd], '\n') == -1 && i > 0)
+	{
+		i = read(fd, buff[fd], BUFFER_SIZE);
+		if (i <= 0)
+			return (txt);
+		buff[fd][i] = '\0';
+		txt = ft_join(txt, buff[fd]);
+	}
+	return (txt);
+}
---
+++ libft/ft_gnl/get_next_line.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lsidan <lsidan@student.42lyon.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/01/20 13:47:06 by lsidan            #+#    #+#             */
+/*   Updated: 2022/01/20 14:27:33 by lsidan           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+# ifndef MAX_FD
+#  define MAX_FD 4096
+# endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include "../libft.h"
+
+char	*get_next_line(int fd);
+int		ft_strlenn(char *a);
+char	*ft_join(char *a, char *b);
+int		ft_search(char *a, char c);
+void	ft_cut(char *a);
+
+#endif
