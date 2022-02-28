50dfc1e489dfb8673f86a3b9ead4ae6771ac7fb6
Logan SIDAN <logan.sidan@icloud.com>
2022-02-28 21:16:40 +0100

    OMG Split got pimp, add "exit" command, s. d. quotes : DONE (only for split lol)

--- srcs/parser/split.c
+++ srcs/parser/split.c
@@ -6,36 +6,38 @@
 /*   By: lsidan <lsidan@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/28 20:46:08 by lsidan            #+#    #+#             */
-/*   Updated: 2022/02/28 20:56:35 by lsidan           ###   ########.fr       */
+/*   Updated: 2022/02/28 21:13:36 by lsidan           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../incl/minishell.h"
 
-static int	is_charset(char str, char *charset)
+static int	is_charset(char c, char *charset)
 {
-	while (*charset)
+	int	i;
+
+	i = -1;
+	while (charset[++i])
 	{
-		if (str == *charset)
+		if (c == charset[i])
 			return (1);
-		charset++;
 	}
 	return (0);
 }
 
-static int	ft_wordlend(char *str, char *charset)
+static int	w_len(char *str, char *charset)
 {
 	int		i;
 	char	c;
 
 	i = -1;
-	c = '\0';
+	c = 0;
 	while (str[++i])
 	{
 		if (!c && (str[i] == '\'' || str[i] == '"'))
 			c = str[i];
 		else if (c == str[i])
-			c = '\0';
+			c = 0;
 		if (!c && is_charset(str[i], charset))
 			return (i);
 	}
@@ -59,16 +61,16 @@ static int	count_w(char *s, char *charset)
 	char	c;
 
 	w = 0 ;
-	i = 0;
-	c = '\0';
+	i = -1;
+	c = 0;
 	skip_char(s, &i, charset);
-	while (s[i])
+	while (s[++i])
 	{
 		if (!c && (s[i] == '\'' || s[i] == '\"'))
 			c = s[i];
 		else if (c == s[i])
-			c = '\0';
-		if (is_charset(s[i++], charset) && !c && ft_strlen(s + 1) > 0)
+			c = 0;
+		if (is_charset(s[i], charset) && !c && ft_strlen(s + 1) > 0)
 		{
 			w++;
 			while (s[i] && s[i] == charset[0])
@@ -81,7 +83,7 @@ static int	count_w(char *s, char *charset)
 	return (w);
 }
 
-static char	*ft_strduppd(char *src, int len)
+static char	*strdup_pimp(char *str, int len)
 {
 	char	*dst;
 	int		i;
@@ -90,8 +92,8 @@ static char	*ft_strduppd(char *src, int len)
 	dst = gc_malloc((len + 1) * sizeof(char));
 	if (!dst)
 		return (0);
-	while (++i < len && src[i])
-		dst[i] = src[i];
+	while (++i < len && str[i])
+		dst[i] = str[i];
 	dst[i] = 0;
 	return (dst);
 }
@@ -115,8 +117,8 @@ char	**split(char const *s, char c)
 	{
 		while (is_charset((char)*s, charset))
 			s++;
-		len = ft_wordlend((char *)s, charset);
-		splitted[i] = ft_strduppd((char *)s, len);
+		len = w_len((char *)s, charset);
+		splitted[i] = strdup_pimp((char *)s, len);
 		if (!splitted[i])
 			return (ft_malloc_error(splitted));
 		s += len;
