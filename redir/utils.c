48abaddcda2f0a05a3e1d9bef25e74adc58702a4
Esteban Mortier <emortier@z4r3p7.42lyon.fr>
2022-03-09 13:27:06 +0100

    Fixed append with lst_output

--- redir/utils.c
+++ redir/utils.c
@@ -6,7 +6,7 @@
 /*   By: emortier <emortier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/08 09:26:25 by emortier          #+#    #+#             */
-/*   Updated: 2022/03/09 13:04:21 by emortier         ###   ########.fr       */
+/*   Updated: 2022/03/09 13:24:42 by emortier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	*ft_redirects(t_cmd args)
 	if (args.state_out == 1 && ft_strcmp(args.output->content, ""))
 		fds[1] = open(args.output->content, O_RDWR | O_CREAT | O_TRUNC, 0777);
 	else if (args.state_out == 2)
-		fds[1] = open(ft_lstlast(args.apppend)->content, \
+		fds[1] = open(args.output->content, \
 		O_APPEND | O_WRONLY | O_CREAT, 0777);
 	return (fds);
 }
