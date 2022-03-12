2ff3526dd4eb6edf3b8bdef637bc0c710da241ab
Logan SIDAN <logan.sidan@icloud.com>
2022-03-12 17:49:25 +0100

    Fix conflict

---
+++ hello.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  12/03/2022 16:46:57
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int	main(void)
+{
+	printf("test");
+}
