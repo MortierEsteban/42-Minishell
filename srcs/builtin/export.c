16e746b012e9ba5464eca6d9830af7e03cad8ffc
Esteban Mortier <emortier@z4r3p1.42lyon.fr>
2022-03-14 13:36:44 +0100

    env isn't modifiable, gotta put mini_env in t_cmd

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: emortier <emortier@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/12 13:54:19 by emortier          #+#    #+#             */
-/*   Updated: 2022/03/12 17:04:26 by emortier         ###   ########.fr       */
+/*   Updated: 2022/03/14 13:18:40 by emortier         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,19 +30,88 @@ char	**ft_envcpy(char **env)
 	return (cpy);
 }
 
+char	**ft_sort_env(char **env)
+{
+	char	**sorted;
+	char	*swp;
+	int		i;
+
+	i = 0;
+	sorted = ft_envcpy(env);
+	while (sorted[i] && sorted[i + 1])
+	{
+		if (ft_strcmp(sorted[i], sorted[i + 1]) > 0)
+		{
+			swp = sorted[i];
+			sorted[i] = sorted[i + 1];
+			sorted[i + 1] = swp;
+			i = 0;
+		}
+		else
+			i++;
+	}
+	return (sorted);
+}
+
+int	ft_cmp_env_varname(char *env, char *var)
+{
+	int	i;
+
+	i = 0;
+	while (env[i] && var[i] && var[i] != '=' && env[i] != '=')
+	{
+		if (env[i] != var[i])
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_find_var(char **env, char *var)
+{
+	int	i;
+
+	i = -1;
+	while(env[++i])
+		if (!ft_cmp_env_varname(env[i], var))
+			return (env[i]);
+	return (NULL);
+}
+
 int	ft_export(char **env, t_cmd cmd)
 {
 	char	**sorted;
+	char	*tmp;
 	int		i;
 
 	(void)cmd;
 	sorted = ft_sort_env(env);
 	i = -1;
-	while (sorted[++i])
+	if (cmd.cmd[1] == NULL)
 	{
-		ft_putstr_fd("declare -x ", 1);
-		ft_putstr_fd(sorted[i], 1);
-		ft_putstr_fd("\n", 1);
+		while (sorted[++i])
+		{
+			ft_putstr_fd("declare -x ", 1);
+			ft_putstr_fd(sorted[i], 1);
+			ft_putstr_fd("\n", 1);
+		}
 	}
-	return (0);
+	else
+	{
+		while(cmd.cmd[++i])
+		{
+			tmp = ft_find_var(env, cmd.cmd[i]);
+			if (tmp)
+			{
+				env = ft_envcpy(env);
+				if ((tmp = ft_find_var(env, "USER")))
+					tmp = ft_strdup("USER=lsidan");
+			}
+			else
+			{
+				dprintf(2, "weee");
+			}
+		}
+	}
+	return(0);
 }
